0c2feb4839bf71a81dd40090c759aab81f09749a
Maija Lehtosaari <maija.lehtosaari@student.lut.fi>
2024-11-13 00:31:58 +0200

    Added LauncherActivity and it's layout. Created functionality for the buttons, changed manifest so the application starts from launcher view

---
+++ app/src/main/java/com/example/mobileproject/LauncherActivity.java
@@ -0,0 +1,44 @@
+package com.example.mobileproject;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        Button secondActivityBtn = (Button)findViewById(R.id.secondActivityBtn);
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                startIntent.putExtra("com.example.mobileproject.SOMETHING", "HELLO WORLD!");
+                startActivity(startIntent);
+
+            }
+        });
+        Button googleBtn = (Button)findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String google = "http://www.google.com";
+                Uri webaddress = Uri.parse(google);
+
+                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW, webaddress);
+                if (gotoGoogle.resolveActivity(getPackageManager()) != null) {
+                    startActivity(gotoGoogle);
+                }
+            }
+        });
+    }
+
+}
