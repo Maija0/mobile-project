0fa7c5cf14fa353fd0f08e61a03791490b3bdb9d
Maija Lehtosaari <maija.lehtosaari@student.lut.fi>
2024-11-19 16:22:39 +0200

    Added dark mode switch to change contrasting colors in hello world activity

--- app/src/main/java/com/example/mobileproject/SecondActivity.java
+++ app/src/main/java/com/example/mobileproject/SecondActivity.java
@@ -1,10 +1,13 @@
 package com.example.mobileproject;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -21,5 +24,20 @@ public class SecondActivity extends AppCompatActivity {
             String text = getIntent().getExtras().getString("com.example.mobileproject.SOMETHING");
             tv.setText(text);
         }
+        Switch colorSwitch = findViewById(R.id.colorSwitch);
+        TextView tv = (TextView) findViewById(R.id.textView);
+        final ConstraintLayout mainLayout = findViewById(R.id.main);
+
+        colorSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                mainLayout.setBackgroundColor(Color.BLACK);
+                tv.setTextColor(Color.WHITE);
+                colorSwitch.setTextColor(Color.WHITE);
+            }   else    {
+                mainLayout.setBackgroundColor(Color.WHITE);
+                tv.setTextColor(Color.BLACK);
+                colorSwitch.setTextColor(Color.BLACK);
+            }
+        });
     }
 }
